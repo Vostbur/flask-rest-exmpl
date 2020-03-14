9e139a2cf4acba55ece5ff2d620ec35345da5c5d
Alexey Rubtsov <vostbur@gmail.com>
2020-03-14 12:38:38 +0300

    add autoincrement field

--- init_db.py
+++ init_db.py
@@ -1,14 +1,16 @@
+import os.path
 from todo_app import db, Tasks
 
 
-# db.create_all()
+if not os.path.exists('app.db'):
+    db.create_all()
 
-# test_task_1 = Tasks(id=1, title='Buy something', description='milk, cheese, pizza')
-# test_task_2 = Tasks(id=2, title='Learn python', description='find good tutorial')
-#
-# db.session.add(test_task_1)
-# db.session.add(test_task_2)
-# db.session.commit()
+    test_task_1 = Tasks(title='Buy something', description='milk, cheese, pizza')
+    test_task_2 = Tasks(title='Learn python', description='find good tutorial')
+
+    db.session.add(test_task_1)
+    db.session.add(test_task_2)
+    db.session.commit()
 
 all_task = Tasks.query.all()
 task_2 = Tasks.query.filter_by(id=2).first()
